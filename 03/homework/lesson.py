a34c14323bf67b805a87959f5a687ac2bdf3f76f
nikoloz.mikeladze <nikolozm33@gmail.com>
2024-09-13 23:53:12 +0400

    commit

---
+++ 03/homework/lesson.py
@@ -0,0 +1,33 @@
+#ცვლადები
+
+num1=1
+num2=2
+num3=3
+num4=4
+num5=5
+num6=6
+num7=7
+num8=8
+num9=9
+num10=10
+#მაგალითები
+
+print(num1 * num6)
+print(num3 / num9)
+print(num10 - num5)
+print(num3 + num8)
+print(num2 * num2)
+print(num4 + num6)
+print(num10 / num5)
+print(num3 - num10)
+print(num3 + num6)
+print(num3 * num10)
+print(num1 + num6)
+
+name = "nika"
+surname = "mikeladze"
+country = "georgia"
+age = 14
+hobby = "MTB driving"
+
+print(name, surname, country, str(age), hobby)
